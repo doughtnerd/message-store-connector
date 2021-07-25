71fab7ff822649a9e9cf6b0d76574b871fe48615
Chris Carlson <doughtnerd@gmail.com>
2021-07-25 00:45:54 -0700

    Updated package

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM node:latest AS base
+
+WORKDIR /usr/src
+
+FROM base as dependencies
+COPY package*.json .
+COPY yarn.lock .`
+RUN yarn install
+
+FROM dependencies AS dev
+COPY . .
+
+# FROM dev as build
+# RUN yarn run build
+
+# FROM node:alpine AS prod
---
+++ __tests__/message-store-connector.test.ts
@@ -0,0 +1,64 @@
+import { v4 as uuid } from "uuid";
+import { connect } from "../src/message-db-client";
+import { MessageStoreConfig } from "../src/types/message-store-config.type";
+import { MessageStore } from "../src/types/message-store.type";
+import { Message } from "../src/types/message.type";
+
+describe("Message Store Connector", () => {
+  const messageStoreConfig: MessageStoreConfig = {
+    messageStoreHost: "message_store",
+    messageStorePassword: "password",
+    logger: console,
+  };
+  let messageStore: MessageStore;
+
+  beforeAll(async () => {
+    messageStore = await connect(messageStoreConfig);
+  });
+
+  test("Can write a message and retrieve the last one written to a stream", async () => {
+    const streamId = uuid();
+    const messageId = uuid();
+    await messageStore.writeMessage(`testStream-${streamId}`, {
+      id: messageId,
+      type: "TestEvent",
+      data: {},
+      metadata: {},
+    });
+    const message = await messageStore.getLastStreamMessage(
+      `testStream-${streamId}`
+    );
+
+    expect(message[0].id).toEqual(messageId);
+  });
+
+  test("Calls the correct handler for a message type in a subscription", (done) => {
+    const streamId = uuid();
+    const messageId = uuid();
+    messageStore
+      .writeMessage(`testStream-${streamId}`, {
+        id: messageId,
+        type: "TestEvent",
+        data: {},
+        metadata: {},
+      })
+      .then(() => {
+        messageStore.subscribeToStream(
+          "abc",
+          `testStream-${streamId}`,
+          {
+            TestEvent: (message: Message, context: any) => {
+              expect(message.id).toEqual(messageId);
+              done();
+              return Promise.resolve(true);
+            },
+          },
+          { pollingInterval: 500 }
+        );
+      });
+  });
+
+  afterAll(async () => {
+    await messageStore.disconnect();
+  });
+});

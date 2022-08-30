552a16fdb393f15208bfcacf93f2673a893b3920
Chris Carlson <doughtnerd@gmail.com>
2022-08-29 23:44:59 -0700

    Updated default projection messagetype

--- src/types/entity-projection.type.ts
+++ src/types/entity-projection.type.ts
@@ -1,8 +1,8 @@
 import { Message } from "./message.type";
 
-export type ProjectionHandlerFunc<EntityType extends {} | Function, MessageDataType, MessageType extends string> = (entity: EntityType, message: Message<MessageDataType, MessageType>) => EntityType
+export type ProjectionHandlerFunc<EntityType extends {} | Function, MessageDataType, MessageType extends string = any> = (entity: EntityType, message: Message<MessageDataType, MessageType>) => EntityType
 
-export type EntityProjection<EntityType, MessageDataType, MessageTypes extends string> = {
+export type EntityProjection<EntityType, MessageDataType, MessageTypes extends string = any> = {
   projectionName: string;
   entity: EntityType;
   handlers: Partial<{
